0746bf9d0f3f2f79e187a7997fd30a408dccede1
Peter Pajor <peter.pajor@cheppers.com>
2019-12-16 01:02:00 +0100

    Update user-data with latest minecraft server version.

--- user-data.sh
+++ user-data.sh
@@ -9,7 +9,7 @@ mkdir -p /home/ec2-user/minecraft-server && cd $_
 
 curl -LSs -O https://raw.githubusercontent.com/district13labs/mc-aws/develop/assets/mc-server-components/eula.txt
 curl -LSs -O https://raw.githubusercontent.com/district13labs/mc-aws/develop/assets/mc-server-components/server.properties
-curl -LSs -O https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar
+curl -LSs -O https://launcher.mojang.com/v1/objects/e9f105b3c5c7e85c7b445249a93362a22f62442d/server.jar
 
 chown -R ec2-user:ec2-user /home/ec2-user
 
