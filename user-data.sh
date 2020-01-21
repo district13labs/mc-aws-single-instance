32fefbca2dd6127d0ebe1083b23e754774f0c3f8
Peter Pajor <pajor.peter94@gmail.com>
2020-01-21 20:47:29 +0100

    Update minecraft version

--- user-data.sh
+++ user-data.sh
@@ -9,7 +9,7 @@ mkdir -p /home/ec2-user/minecraft-server && cd $_
 
 curl -LSs -O https://raw.githubusercontent.com/district13labs/mc-aws/develop/assets/mc-server-components/eula.txt
 curl -LSs -O https://raw.githubusercontent.com/district13labs/mc-aws/develop/assets/mc-server-components/server.properties
-curl -LSs -O https://launcher.mojang.com/v1/objects/e9f105b3c5c7e85c7b445249a93362a22f62442d/server.jar
+curl -LSs -O https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
 
 chown -R ec2-user:ec2-user /home/ec2-user
 
